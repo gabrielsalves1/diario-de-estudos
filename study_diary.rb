87c8c3845c5bcae49d3ec0baa9c3f1912074238d
gabrielsalves1 <gabrielsalves2k18@gmail.com>
2021-11-17 17:45:25 -0300

    Commit inicial

---
+++ study_diary.rb
@@ -0,0 +1,9 @@
+puts "[1] Cadastrar um item para estudar"
+puts "[2] Ver todos os itens cadastrados"
+puts "[3] Buscar um item de estudo"
+puts "[4] Sair"
+
+print "\nEscolha uma opção: "
+
+opcao = gets.chomp
+puts opcao
