194b78199e649708a16f2b2aadf300862cc3fec7
gabrielsalves1 <gabrielsalves2k18@gmail.com>
2021-11-18 08:15:18 -0300

    Listar por categoria

--- study_diary.rb
+++ study_diary.rb
@@ -11,21 +11,15 @@ def cadastraItem(nome, categoria, itens)
 end
 
 def menu(itens)
-
     puts "\n[1] Cadastrar um item para estudar"
     puts "[2] Ver todos os itens cadastrados"
     puts "[3] Buscar um item de estudo"
     puts "[4] Sair"
     
     print "\nEscolha uma opção: "
-    
     opcao = gets.chomp().to_i
 
-    if opcao == 4
-        puts "Encerrando aplicação..."
-    end
-
-    while opcao != 4
+    while true
         if opcao == 1
             print "Digite o nome do item: "
             nomeItem = gets.chomp
@@ -33,7 +27,6 @@ def menu(itens)
             categoriaItem = gets.chomp
 
             cadastraItem(nomeItem, categoriaItem, itens)
-
             menu(itens)
         elsif opcao == 2
             itens.each_with_index {|item, index|
@@ -42,21 +35,42 @@ def menu(itens)
 
             menu(itens)
         elsif opcao == 3
+            print "Deseja buscar por categoria? (S/N) "
+            buscarPorCategoria = gets.chomp().downcase
+
+            while buscarPorCategoria != "n" && buscarPorCategoria != "s"
+                puts "Resposta inválida, utilizar S ou N"
+
+                print "Deseja buscar por categoria? (S/N) "
+                buscarPorCategoria = gets.chomp().downcase
+            end
+
             print "Digite uma palavra contida no item a ser procurado: "
             palavra = gets.chomp()
-
-            itens.each_with_index {|item, index|
-                if item.nome == palavra || item.categoria == palavra
-                    puts "#{index}: Nome: #{item.nome}, Categoria: #{item.categoria}"
-                end
-            }
+            
+            if buscarPorCategoria == "n"
+                itens.each_with_index {|item, index|
+                    if item.nome == palavra || item.categoria == palavra
+                        puts "\n#{index}: Nome: #{item.nome}, Categoria: #{item.categoria}"
+                    end
+                }
+            elsif buscarPorCategoria == "s"
+                itens.each_with_index {|item, index|
+                    if item.categoria == palavra
+                        puts "\n#{index}: Nome: #{item.nome}, Categoria: #{item.categoria}"
+                    end
+                }
+            end
 
             menu(itens)
+        elsif opcao == 4
+            puts "Encerrando aplicação..."
         else
             puts "Opção inválida, selecione uma opção de 1 à 4"
             
             menu(itens)
         end
+        break
     end
 end
 
